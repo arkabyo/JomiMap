9131f9a608ee90c36f13c72535ab764f26447449
arkabyo <mail@avjitroy.com>
2025-05-17 05:11:58 -0400

    sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,27 @@
+// sw.js
 const CACHE_NAME = "jomimap-v1";
 const ASSETS = [
-  "/JomiMap/index.html",
-  "/JomiMap/js/script2.js",
-  "/JomiMap/css/styles.css",
-  "/JomiMap/css/bootstraps/bootstrap.min.css",
-  "/JomiMap/css/bootstraps/bootstrap-icons.css",
-  "/JomiMap/images/JomiMap-Logo.svg",
-  "/JomiMap/favicon-96x96.png"
+  "index.html",
+  "js/script2.js",
+  "css/styles.css",
+  "css/bootstraps/bootstrap.min.css",
+  "css/bootstraps/bootstrap-icons.css",
+  "images/JomiMap-Logo.svg",
+  "favicon-96x96.png"
 ];
 
-self.addEventListener("install", e =>
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)))
-);
+self.addEventListener("install", evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .catch(err => console.error("SW cache failed:", err))
+  );
+});
 
-self.addEventListener("fetch", e =>
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
-);
+self.addEventListener("fetch", evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cached => {
+      return cached || fetch(evt.request);
+    })
+  );
+});
